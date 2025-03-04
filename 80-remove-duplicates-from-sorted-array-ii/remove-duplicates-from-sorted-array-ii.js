3501fd121b129f6a55ea5c73ca526ee34c1eb530
Masood Saaed <36909914+MasoodMS95@users.noreply.github.com>
2025-03-04 15:50:06 -0800

    Time: 56 ms (81.22%) | Memory: 58.1 MB (15.02%) - LeetSync

---
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let i = 2, j = 2;
+    let currCount = 1;
+    if(nums.length===2) return nums.length;
+    while(j < nums.length){
+        if(nums[i-2] !== nums[j]){
+            nums[i] = nums[j];
+            i++;
+        }
+        j++;
+    }
+    return i;
+};
