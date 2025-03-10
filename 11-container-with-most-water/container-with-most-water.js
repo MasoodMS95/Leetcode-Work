9e8f08aeb00d30b6088d53f846c90666d7a48d10
Masood Saaed <36909914+MasoodMS95@users.noreply.github.com>
2025-03-10 14:08:01 -0700

    Time: 3 ms (53.82%) | Memory: 61.9 MB (27.50%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let left = 0, right = height.length -1, best = -1;
+    while(left < right){
+        let length = right-left;
+        let currHeight = Math.min(height[left], height[right]);
+        let area = length * currHeight;
+        if(best < area){
+            best = area;
+        }
+        if(height[left] > height[right]){
+            right--;
+        }
+        else{
+            left++;
+        }
+    }
+    return best;
+};
