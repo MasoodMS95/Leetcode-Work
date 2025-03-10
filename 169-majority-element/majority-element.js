343d9dc6c89bc44e9f265fb36d8f676cb968bccc
Masood Saaed <36909914+MasoodMS95@users.noreply.github.com>
2025-03-10 13:32:23 -0700

    Time: 7 ms (31.05%) | Memory: 60.1 MB (5.36%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -1,12 +1,27 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
-
-
 var majorityElement = function(nums) {
+    let currCount = 1;
+    let bestCount = 0;
+    let bestNum = -1;
+    let currNum = nums[0];
     nums.sort((a,b)=>a-b);
-    if(nums.length < 3){return nums[nums.length - 1]}
-    let mid = parseInt((nums.length/2), 10)
-    return nums[mid]
+    for(let num = 0; num < nums.length; num++){
+        if(nums[num] !== currNum || num === (nums.length -1)){
+            if(num === nums.length -1){
+                currCount++;
+            }
+            if(currCount > bestCount){
+                bestCount = currCount;
+                bestNum = currNum;
+            }
+            else if(currCount === bestCount){
+                bestNum = currNum < bestNum ? bestNum:currNum
+            }
+            currNum = nums[num];
+            currCount = 1;
+        }
+        else{
+            currCount++;
+        }
+    }
+    return bestNum
 };
