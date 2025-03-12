e21b7e6d0434b8aeb80768a41cd5668f5815f140
Masood Saaed <36909914+MasoodMS95@users.noreply.github.com>
2025-03-12 12:51:52 -0700

    Time: 3 ms (53.95%) | Memory: 62.9 MB (21.93%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.js
+++ 11-container-with-most-water/container-with-most-water.js
@@ -3,20 +3,14 @@
  * @return {number}
  */
 var maxArea = function(height) {
-    let left = 0, right = height.length -1, best = -1;
+    let left = 0; right = height.length-1, best = -1;
     while(left < right){
-        let length = right-left;
+        let length = right - left;
         let currHeight = Math.min(height[left], height[right]);
         let area = length * currHeight;
-        if(best < area){
-            best = area;
-        }
-        if(height[left] > height[right]){
-            right--;
-        }
-        else{
-            left++;
-        }
+        best = best > area ? best : area;
+        if(height[left] > height[right]){right--;}
+        else{left++}
     }
     return best;
 };
