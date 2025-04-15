dc82d356bddef440eb4e8ae43458bb021360f469
Masood Saaed <36909914+MasoodMS95@users.noreply.github.com>
2025-04-14 22:08:59 -0700

    Time: 24 ms (69.02%) | Memory: 60.3 MB (25.42%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.js
+++ 76-minimum-window-substring/minimum-window-substring.js
@@ -1,43 +1,46 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
 var minWindow = function(s, t) {
-    let queue = [];
-    let visited = {};
-    let needChars = {};
-
-    for (char of t) {
-        if (needChars[char]) {
-            needChars[char]++;
-        } else {
-            needChars[char] = 1;
-        }
-        visited[char] = 0;
-    }
-
-    let needCount = Object.keys(needChars).length;
-    let haveCount = 0;
-    let currShortest = "";
+    let minString = "";
+    let start = 0;
     let currIndex = 0;
+    
+    let needChars = {};
+    for(char of t){
+        needChars[char] = needChars[char] ? needChars[char] + 1 : 1;
+    }
+    let needCount = Object.keys(needChars).length;
 
-    while (currIndex < s.length) {
-        if (t.indexOf(s[currIndex]) !== -1) {
-            visited[s[currIndex]]++;
-            if (visited[s[currIndex]] === needChars[s[currIndex]]) {
+    let haveChars = {};
+    for(char of t){
+        haveChars[char] = 0;
+    }
+    let haveCount = 0;
+
+    while(currIndex < s.length){
+        let cChar = s[currIndex];
+        if(t.includes(cChar)){
+            haveChars[cChar]++;
+            if(haveChars[cChar] === needChars[cChar]){
                 haveCount++;
             }
-
-            queue.push(currIndex);
-
-            while (haveCount === needCount) {
-                currShortest = currShortest.length > (currIndex - queue[0]) + 1 || currShortest.length === 0 ? s.slice(queue[0], currIndex + 1) : currShortest;
-
-                let removed = queue.shift();
-                if (visited[s[removed]] === needChars[s[removed]]) {
-                    haveCount--;
-                }
-                visited[s[removed]]--;
+        }
+        while(haveCount === needCount){
+            let checkString = s.slice(start, currIndex+1);
+            
+            if(minString.length > checkString.length || minString.length === 0){
+                minString = checkString;
             }
+            if(t.includes(s[start])){
+                haveChars[s[start]]--;
+                if(haveChars[s[start]] < needChars[s[start]]){haveCount--;}
+            }
+            start++;
         }
         currIndex++;
     }
-
-    return currShortest;
+    return minString;
 };
