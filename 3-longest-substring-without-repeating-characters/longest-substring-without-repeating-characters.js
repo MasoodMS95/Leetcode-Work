0c02a3ffe2ad7ec3aed1fdd7ca3da84cefe28d30
Masood Saaed <36909914+MasoodMS95@users.noreply.github.com>
2025-07-04 09:42:00 -0700

    Time: 5 ms (80.57%) | Memory: 58.1 MB (56.97%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -3,21 +3,35 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let longestCount = 0;
-    if(s.length === 1) return 1;
-    let subString = new Set();
-    let start = 0;
-    let curr = 0;
-    while(curr < s.length){
-        if(!subString.has(s[curr])){
-            subString.add(s[curr])
-            longestCount = Math.max(longestCount, subString.size);
-            curr++;
+    if(s.length === 0){
+        return 0;
+    }
+    if(s.length === 1){
+        return 1;
+    }
+    if(s.length === 2){
+        if(s[0] === s[1]){
+            return 1;
         }
-        else{
-            subString.delete(s[start]);
+        return 2;
+    }
+
+    let start = 0;
+    let end = 1;
+    let longestSubString = 0;
+    let seen = new Set();
+    seen.add(s[start])
+    while(end < s.length){
+        while(seen.has(s[end])){
+            seen.delete(s[start]);
             start++;
         }
+        seen.add(s[end]);
+        let currLen = (end - start) + 1;
+        if(currLen > longestSubString){
+            longestSubString = currLen;
+        }
+        end++;
     }
-    return longestCount;
+    return longestSubString;
 };
