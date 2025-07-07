2631e2beb571cf2b9e620876588cb6b911794aea
Masood Saaed <36909914+MasoodMS95@users.noreply.github.com>
2025-07-07 12:32:47 -0700

    Time: 37 ms (54.48%) | Memory: 73.2 MB (21.13%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -3,28 +3,22 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
+    let foundValues = [];
     nums.sort((a,b)=>a-b);
-    let foundSums = [];
-    for(let x = 0; x < nums.length-2; x++){
-        if (x > 0 && nums[x] === nums[x - 1]) continue;
+    for(let x = 0; x < nums.length - 2; x++){
+        if(x > 0 && nums[x] === nums[x-1]){continue;}
         let left = x+1;
         let right = nums.length-1;
         while(left < right){
-            let sum = nums[left] + nums[x] + nums[right];
-            if(sum === 0){
-                foundSums.push([nums[x], nums[left], nums[right]])
+            let currAddVal = nums[x] + nums[left] + nums[right];
+            if(currAddVal === 0){
+                foundValues.push([nums[x], nums[left], nums[right]]);
                 while(left < right && nums[left] === nums[left+1]){left++;}
                 while(left < right && nums[right] === nums[right-1]){right--;}
-                left++;
-                right--;
             }
-            else if(sum < 0){
-                left++;
-            }
-            else{
-                right--;
-            }
+            if(currAddVal > 0){right--;}
+            else{left++;}
         }
     }
-    return foundSums;
-};
+    return foundValues;
+}
