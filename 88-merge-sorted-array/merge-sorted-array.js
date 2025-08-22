8c166028743352271b43e614197253b65ea21751
Masood Saaed <36909914+MasoodMS95@users.noreply.github.com>
2025-08-22 13:04:58 -0700

    Time: 0 ms (100.00%) | Memory: 56.1 MB (13.95%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -6,26 +6,29 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    let largestIndexLeft = m-1;
-    let largestIndexRight = n-1;
-    let currIndex = (m+n)-1
-    let valToPlace;
-    while(currIndex >= 0){
-        if(largestIndexRight < 0){
+    let n1Index = m-1;
+    let n2Index = n-1;
+    let currPointer = (m+n)-1;
+
+    while(currPointer >= 0){
+        if(n2Index < 0){
             break;
         }
-        if(nums1[largestIndexLeft] > nums2[largestIndexRight]){
-            valToPlace = nums1[largestIndexLeft];
-            nums1[largestIndexLeft] = 0;
-            largestIndexLeft--;
+        let valToPlace;
+        let n1 = nums1[n1Index];
+        let n2 = nums2[n2Index]
+        if(n1 > n2){
+            valToPlace = n1;
+            nums1[n1Index] = 0;
+            n1Index--;
         }
         else{
-            valToPlace = nums2[largestIndexRight];
-            largestIndexRight--;
+            valToPlace = n2;
+            n2Index--;
         }
-        if(nums1[currIndex] === 0){
-            nums1[currIndex] = valToPlace;
+        if(nums1[currPointer] === 0){
+            nums1[currPointer] = valToPlace;
         }
-        currIndex--;
+        currPointer--;
     }
 };
