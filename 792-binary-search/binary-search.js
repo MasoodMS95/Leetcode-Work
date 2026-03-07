0c858e0fc771586967e56685534b56ae27909d9b
Masood Saaed <36909914+MasoodMS95@users.noreply.github.com>
2026-03-06 23:38:06 -0800

    Time: 0 ms (100.00%) | Memory: 58.1 MB (67.29%) - LeetSync

--- 792-binary-search/binary-search.js
+++ 792-binary-search/binary-search.js
@@ -4,18 +4,25 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let left = 0;
-    let right = nums.length-1;
+    let [left, right] = [0, nums.length-1];
+    if(nums.length === 1){
+        if(nums[0] === target){
+            return 0;
+        }
+        else{
+            return -1;
+        }
+    }
     while(left <= right){
-        let mid = Math.floor((right + left) / 2);
+        let mid = Math.floor((left+right) / 2);
         if(nums[mid] === target){
             return mid;
         }
-        else if(nums[mid] < target){
-            left =  mid + 1
+        if(target < nums[mid]){
+            right = mid-1;
         }
         else{
-            right = mid - 1
+            left = mid+1;
         }
     }
     return -1;
