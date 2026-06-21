941442763053a3240798ecffe35646df0125c37d
Masood Saaed <36909914+MasoodMS95@users.noreply.github.com>
2026-06-21 00:12:35 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (61.12%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -8,10 +8,13 @@ class Solution:
         if(len(nums) == 2):
             return max(nums[0], nums[1])
 
-        dp = []
-        dp.append(nums[0])
-        dp.append(max(nums[0], nums[1]))
+        prev = nums[0]
+        curr = max(nums[0], nums[1])
         
         for x in range(2, len(nums)):
-            dp.append(max(dp[x-1], dp[x-2] + nums[x]))
-        return dp[-1]
+            currValue = nums[x] + prev
+            prev = curr
+            if(curr < currValue):
+                curr = currValue
+
+        return curr
