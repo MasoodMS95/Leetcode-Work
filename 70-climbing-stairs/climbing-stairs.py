c5b6afb3f5b566c2bf9ff03ccf7438b5463b473b
Masood Saaed <36909914+MasoodMS95@users.noreply.github.com>
2026-06-23 19:57:37 -0700

    Time: 0 ms (100.00%) | Memory: 19.2 MB (52.13%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,12 +1,15 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        prevSteps = 1
-        currSteps = 1
-        steps = 1
-        if(n < 2):
-            return currSteps
-        for x in range(2, n+1):
-            newCount = currSteps + prevSteps
-            prevSteps = currSteps
-            currSteps = newCount
-        return currSteps
+        if(n == 2):
+            return 2
+        if(n == 1):
+            return 1
+
+        dp = []
+        dp.append(1)
+        dp.append(2)
+
+        for x in range(2, n):
+            dp.append(dp[x-2] + dp[x-1])
+        
+        return dp[-1]
